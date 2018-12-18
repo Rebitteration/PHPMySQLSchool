f9f3d96b7bd3d3f18a3a023bf23f92c9eb4de4ee
Rebitteration <36053415+Rebitteration@users.noreply.github.com>
2018-12-18 09:26:17 +0100

    First Commit :fire:

---
+++ 2.1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Opdracht 2.1</title>
+    </head>
+    <body>
+        <?php
+            echo "Hallo Wereld";
+        ?>
+    </body>
+</html>
