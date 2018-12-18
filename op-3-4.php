c82487898d62246e57488f5a8f4f28481630bfa8
Rebitteration <36053415+Rebitteration@users.noreply.github.com>
2018-12-18 13:11:23 +0100

    Opdracht 3.4 :fire:

---
+++ op-3-4.php
@@ -0,0 +1,6 @@
+<?php
+    $naam = "Piet Jansen";
+    echo "lengte: " . strlen($naam) . "<br>\n" . "voornaam: " . substr($naam,0,4) . "<br>\n" . "achternaam: " . substr($naam, 5) . "<br>\n";
+
+
+?>
